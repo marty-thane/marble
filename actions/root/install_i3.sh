e54ea6b8335314661ce3829ab588911163efbcd7
Marty Thane <mzf@tuta.io>
2024-10-02 18:07:09 +0200

    do not install dmenu

--- actions/root/install_i3.sh
+++ actions/root/install_i3.sh
@@ -3,5 +3,3 @@ pkg_install $(cat $STATIC_DIR/progs)
 mkdir -pv $HOME/.local/src/
 git clone https://github.com/marty-thane/sent.git $HOME/.local/src/sent/
 make -C $HOME/.local/src/sent/ install clean
-git clone https://github.com/marty-thane/dmenu.git $HOME/.local/src/dmenu/
-make -C $HOME/.local/src/dmenu/ install clean
