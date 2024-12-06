605bab3a6f5adb71165c8da1e6e349a3c1da8fbd
Marty Thane <mzf@tuta.io>
2024-12-06 21:06:46 +0100

    added pinentry-rofi

--- actions/noroot/setup_gpg.sh
+++ actions/noroot/setup_gpg.sh
@@ -1,4 +1,6 @@
 # Setup GnuPG
 mkdir -pv "$HOME/.local/share/gnupg"
 chmod 700 "$HOME/.local/share/gnupg"
+echo 'pinentry-program /usr/local/bin/pinentry-rofi' > "$HOME/.local/share/gnupg/gpg-agent.conf"
+gpg-connect-agent reloadagent /bye
 gpg --gen-key
--- actions/root/install_i3.sh
+++ actions/root/install_i3.sh
@@ -1,5 +1,7 @@
 # Install i3
 pkg_install $(cat $STATIC_DIR/progs)
 mkdir -pv $HOME/.local/src/
-git clone https://github.com/marty-thane/sent.git $HOME/.local/src/sent/
+git clone https://github.com/marty-thane/sent $HOME/.local/src/sent/
 make -C $HOME/.local/src/sent/ install clean
+git clone https://github.com/plattfot/pinentry-rofi $HOME/.local/src/pinentry-rofi/
+cd $HOME/.local/src/pinentry-rofi/ && autoreconf -vif && ./configure && make check && make install
