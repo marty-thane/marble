6dac8fb7e24b232b64de40d43e32641ec9c32120
Marty Thane <mzf@tuta.io>
2024-12-11 14:28:00 +0100

    install user.js in default profile only

--- actions/noroot/install_addons.sh
+++ actions/noroot/install_addons.sh
@@ -1,7 +1,6 @@
 # Install Firefox addons
 firefox $(cat $STATIC_DIR/addons)
-# install custom user.js
-for dir in $HOME/.mozilla/firefox/*; do
-	[ -d "$dir" ] && wget -nv -O "$dir/user.js" \
+ffdir="$HOME/.mozilla/firefox/"
+ffprofile="$(cat "$ffdir/profiles.ini" | grep -m1 'Default' | cut -d'=' -f2)"
+wget -nv -O "$ffdir/$ffprofile/user.js" \
 	https://raw.githubusercontent.com/arkenfox/user.js/master/user.js
-done
