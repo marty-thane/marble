06aa64d1da398431865f7f6b9912278cf52f6e1a
Marty Thane <mzf@tuta.io>
2024-12-11 14:31:14 +0100

    removed compilated programs from i3 install

--- actions/root/install_i3.sh
+++ actions/root/install_i3.sh
@@ -1,7 +1,2 @@
 # Install i3
 pkg_install $(cat $STATIC_DIR/progs)
-mkdir -pv $HOME/.local/src/
-git clone https://github.com/marty-thane/sent $HOME/.local/src/sent/
-make -C $HOME/.local/src/sent/ install clean
-git clone https://github.com/plattfot/pinentry-rofi $HOME/.local/src/pinentry-rofi/
-cd $HOME/.local/src/pinentry-rofi/ && autoreconf -vif && ./configure && make check && make install
