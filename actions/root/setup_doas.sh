a2d263cff6688e889bcf6d7561cb98c48fb931e1
Marty Thane <mzf@tuta.io>
2024-12-11 14:32:17 +0100

    secure doas.conf by ensuring proper permissions/ownership

--- actions/root/setup_doas.sh
+++ actions/root/setup_doas.sh
@@ -1,5 +1,7 @@
 # Setup doas
 pkg_install opendoas
 cp -v "$STATIC_DIR/doas.conf" '/etc/'
+chown -c root:root /etc/doas.conf
+chmod -c 0400 /etc/doas.conf
 echo 'ignorepkg=sudo' > /etc/xbps.d/nosudo.conf
 pkg_remove sudo
