b66c5ba7a6c5611d49516c2d3d27d15a3d33af09
Marty Thane <mzf@tuta.io>
2024-12-11 15:34:27 +0100

    remove .config before installing dotfiles

--- actions/noroot/install_dotfiles.sh
+++ actions/noroot/install_dotfiles.sh
@@ -1,3 +1,5 @@
 # Install dotfiles
-read -p 'GitHub URL: https://github.com/' gh_path
-git clone "https://github.com/$gh_path" $HOME/.config/
+read -p 'GitHub URL: https://github.com/' ghpath
+confdir="${XDG_CONFIG_HOME:-$HOME/.config}"
+[ -d "$confdir" ] && rm -rvI "$confdir"
+git clone "https://github.com/$ghpath" $confdir
