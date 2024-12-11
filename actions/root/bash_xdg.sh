caf269783a00082994a2b89f111487ba82022a37
Marty Thane <mzf@tuta.io>
2024-12-11 14:33:33 +0100

    changed chmod option in bash xdg

--- actions/root/bash_xdg.sh
+++ actions/root/bash_xdg.sh
@@ -1,7 +1,7 @@
 # Make bash XDG compliant
 mkdir -pv "/etc/profile.d/"
 cp -v "$STATIC_DIR/profile_xdg.sh" "/etc/profile.d/"
-chmod -v 644 "/etc/profile.d/profile_xdg.sh"
+chmod -c 644 "/etc/profile.d/profile_xdg.sh"
 mkdir -pv "/etc/bash/bashrc.d/"
 cp -v "$STATIC_DIR/bashrc_xdg.sh" "/etc/bash/bashrc.d/"
-chmod -v 644 "/etc/bash/bashrc.d/bashrc_xdg.sh"
+chmod -c 644 "/etc/bash/bashrc.d/bashrc_xdg.sh"
